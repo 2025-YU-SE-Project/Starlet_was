06813efc07c2f79fcd23716b11ca0d98f10225e7
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-09-03 16:09:46 +0900

    ref : 미사용 클래스 삭제

--- Starlet_BE/src/main/java/com/example/starlet_be/repository/ConstellationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.starlet_be.repository;
-
-import com.example.starlet_be.entity.Constellation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConstellationRepository extends JpaRepository<Constellation, Long> {
-}
