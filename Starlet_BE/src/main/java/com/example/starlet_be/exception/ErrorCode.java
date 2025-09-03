1c3aff0ac76acc6d3d1ad735af92c7f5c3719e7e
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-09-03 16:49:15 +0900

    feat : 회원가입 관련 에러코드 추가

--- Starlet_BE/src/main/java/com/example/starlet_be/exception/ErrorCode.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/exception/ErrorCode.java
@@ -9,6 +9,10 @@ public enum ErrorCode {
 
     // 유저 관련
     USER_NOT_FOUND(404, "해당 유저를 찾을 수 없습니다."),
+    EMAIL_CONFLICT(409, "이메일이 중복됩니다."),
+    NICKNAME_CONFLICT(409, "닉네임이 중복됩니다."),
+    DUPLICATE_INFO_CONFLICT(409, "이미 사용중인 정보가 있습니다. 이메일과 닉네임 중복검사를 시행하세요."),
+    EMAIL_SEND_FAILED(500, "메일 전송을 실패하였습니다."),
 
 
     // 기타 관련
