b9f6fb9bd055b73284df81773f39910dca513f83
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-09-07 00:20:45 +0900

    feat : 인증 컨트롤러 기본 세팅과 DTO 정의

--- Starlet_BE/src/main/java/com/example/starlet_be/domains/verify/controller/VerifyController.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/verify/controller/VerifyController.java
@@ -1,29 +1,54 @@
 package com.example.starlet_be.domains.verify.controller;
 
+import com.example.starlet_be.domains.verify.reqdto.PasswordResetConfirmDto;
+import com.example.starlet_be.domains.verify.service.VerifyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v2/verify")
+@RequiredArgsConstructor
 public class VerifyController {
 
+    private final VerifyService verifyService;
 
+    // 1. 가입 이메일 인증 받기
+    // 3. 계정생성 이메일 인증 이후 인증된 계정으로 전환
+    @GetMapping("/email")
+    public ResponseEntity<?> emailVerification(@RequestParam String token){
+        return (verifyService.emailVerification(token)) ?
+                ResponseEntity.ok().build() :
+                ResponseEntity.badRequest().build();
+    }
 
 
     // 2. 비밀번호 변경 허용 인증
-
-
-    // 인증 상태가 만료되면 이메일 객체와 인증 객체 삭제
-
-
-    // 3. 계정생성 이메일 인증 이후 인증된 계정으로 전환
-
-
     // 4. 비밀번호 초기화 이메일 인증 이후 비밀번호 변경상태로 전환
+    @GetMapping("/password")
+    public ResponseEntity<?> passwordResetVerification(@RequestParam String token){
+        return (verifyService.passwordResetVerification(token)) ?
+                ResponseEntity.ok().build() :
+                ResponseEntity.badRequest().build();
+    }
 
 
-    // 5. 비밀번호 변경 승인 요청
+    // 가입 인증 상태가 만료되면 이메일 객체와 인증 객체 삭제
+    // 비밀번호 변경 요청이었다면 이거는 그냥 만료시킴
 
 
-    // 6. 새로운 비밀번호 반영
+
+    // 6. 새로운 비밀번호 반영(새로운 비밀번호 생성)
+    @PostMapping("/password-reset/new-password")
+    public ResponseEntity<?> confirmChangePassword(@RequestBody PasswordResetConfirmDto dto){
+        verifyService.updatePassword(dto);
+        return ResponseEntity.ok().build();
+    }
 
 
 }
