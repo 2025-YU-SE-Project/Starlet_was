d89e023ad8a3bd57e7022ccd2f9d86dcb88debd7
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-09-06 23:23:45 +0900

    feat : 인증 객체, 이메일객체와 생명주기 연동

--- Starlet_BE/src/main/java/com/example/starlet_be/domains/verify/entity/Verify.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/verify/entity/Verify.java
@@ -1,10 +1,13 @@
 package com.example.starlet_be.domains.verify.entity;
 
+import com.example.starlet_be.domains.email.entity.Email;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,9 +25,16 @@ public class Verify {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
+    private String token;
+
     @Column(nullable = false)
     private VerifyType type;
 
     @Column
     private LocalDateTime expireTime;
+
+    @OneToOne(mappedBy = "verify", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private Email email;
+    
 }
