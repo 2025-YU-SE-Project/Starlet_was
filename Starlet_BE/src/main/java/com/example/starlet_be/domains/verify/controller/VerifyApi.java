2b65811b84a45537ac2257a16e550db89bdda860
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-09-07 17:54:25 +0900

    feat : 가입 이메일 인증 API 스웨거 작성

---
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/verify/controller/VerifyApi.java
@@ -0,0 +1,46 @@
+package com.example.starlet_be.domains.verify.controller;
+
+import com.example.starlet_be.domains.verify.reqdto.PasswordResetConfirmDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Tag(name = "Verify API", description = "인증 및 보안에 관련된 API 입니다.")
+public interface VerifyApi {
+
+    @Operation(summary = "가입 이메일 인증 링크", description = "이메일로 받는 인증 링크 입니다. 실제로는 메일로 테스트 하시면 됩니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "인증됨"),
+            @ApiResponse(responseCode = "400", description = "인증 상태 타입이 유효하지 않음",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                        "status": 400,
+                                        "message": "인증 상태 타입이 일치하지 않습니다."
+                                    }
+                                    """)
+                    })),
+            @ApiResponse(responseCode = "404", description = "인증 객체 자체가 없음",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                        "status": 404,
+                                        "message": "인증 정보를 찾을 수 없습니다."
+                                    }
+                                    """)
+                    }))
+    })
+    ResponseEntity<?> emailVerification(@RequestParam String token);
+
+
+    ResponseEntity<?> passwordResetVerification(@RequestParam String token);
+
+
+    ResponseEntity<?> confirmChangePassword(@RequestBody PasswordResetConfirmDto dto);
+}
