e0eaec2a1e186e47cf5028ca64cf984bca686675
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-09-08 16:08:14 +0900

    feat : 만료된 인증정보를 검색하는 jpa메소드

--- Starlet_BE/src/main/java/com/example/starlet_be/domains/verify/repository/VerifyRepository.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/verify/repository/VerifyRepository.java
@@ -3,8 +3,12 @@ package com.example.starlet_be.domains.verify.repository;
 import com.example.starlet_be.domains.verify.entity.Verify;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface VerifyRepository extends JpaRepository<Verify, Long> {
     Optional<Verify> findByToken(String token);
+
+    List<Verify> findAllByExpireTimeBefore(LocalDateTime now);
 }
