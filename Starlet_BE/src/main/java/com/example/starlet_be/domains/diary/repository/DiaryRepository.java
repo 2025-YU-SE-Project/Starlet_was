604632dd48d97aca9b87d22bb5480bc5416d34f2
nahyun <nahyun040411@naver.com>
2025-09-10 15:21:35 +0900

    feat : diary repository 작성

---
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/diary/repository/DiaryRepository.java
@@ -0,0 +1,22 @@
+package com.example.starlet_be.domains.diary.repository;
+
+import com.example.starlet_be.domains.diary.entity.Diary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface DiaryRepository extends JpaRepository<Diary, Long> {
+
+    //1. 사용자/날짜의 일기 존재 여부
+    boolean existsByUser_IdAndCreateAt(Long userId, LocalDate date);
+
+    //2. 일기 조회
+    Optional<Diary> findByUser_IdAndCreateAt(Long userId, LocalDate date);
+
+    //3. 달력 안 별 조회
+    List<Diary> findAllByUser_IdAndCreateAtBetweenOrderByCreateAtAsc(
+            Long userId, LocalDate from, LocalDate to
+    );
+}
