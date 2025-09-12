5b32a22c7c106220842f673284a8beaa36cd0874
nahyun <nahyun040411@naver.com>
2025-09-12 18:42:33 +0900

    feat : StarService 별 생성 삭제

--- Starlet_BE/src/main/java/com/example/starlet_be/domains/star/service/StarService.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/star/service/StarService.java
@@ -30,45 +30,6 @@ public class StarService {
     private final StarRepository starRepository;
     private final DiaryRepository diaryRepository;
 
-    @Transactional
-    public void createStar(UserDetails userDetails, DiaryToStarReqDto dto) {
-        // 1. 사용자 조회
-        User user = userRepository.findByEmailAddress(userDetails.getUsername()).orElseThrow(
-                () -> new CustomException(ErrorCode.USER_NOT_FOUND)
-        );
-
-        // 2. 날짜 파싱
-        LocalDate date;
-        try{
-            date = LocalDate.parse(dto.getDate());
-        } catch (Exception e){
-            throw new CustomException(ErrorCode.DATE_PARSE_ERROR);
-        }
-
-
-        // 3. 해당 날짜 일기 들고오기
-        Diary diary = diaryRepository.findByUser_IdAndCreateAt(user.getId(), date).orElseThrow(
-                () -> new CustomException(ErrorCode.DIARY_NOT_FOUND)
-        );
-
-        // 4. 해당 사용자의 해당 일기에 별이 이미 존재하는지
-        if(starRepository.existsByUserAndDiary(user, diary))
-            throw new CustomException(ErrorCode.STAR_ALREADY_EXISTS);
-
-        // 5. 별 만들기
-        Star star = Star.builder()
-                .color(diary.getEmotion().getColor())
-                .x(Math.random())
-                .y(Math.random())
-                .user(user)
-                .diary(diary)
-                .build();
-
-        // 6. 저장
-        starRepository.save(star);
-
-    }
-
     // 별 상세조회
     @Transactional(readOnly = true)
     public StarInfoDto getStar(Long id) {
