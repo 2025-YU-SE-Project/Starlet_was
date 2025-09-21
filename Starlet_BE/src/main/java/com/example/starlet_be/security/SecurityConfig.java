d4456d3298e60c8c8ca7a4af6e75a892e76e182b
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-09-21 19:31:17 +0900

    chore : openai api 테스트 토큰 없이 허용

--- Starlet_BE/src/main/java/com/example/starlet_be/security/SecurityConfig.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/security/SecurityConfig.java
@@ -61,6 +61,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/api/v1/email/**").permitAll()
                         .requestMatchers("/api/v1/verify/**").permitAll()
+                        .requestMatchers("/api/v1/openai/**").permitAll()
                         .requestMatchers("/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs/**",
                                 "/webjars/**", "/error").permitAll()
                         .requestMatchers("/view/**").permitAll()
