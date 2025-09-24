874677287bf7b3ec59784f133155360db53bc9db
nahyun <nahyun040411@naver.com>
2025-09-24 18:08:08 +0900

    feat : S3 Config 수정

--- Starlet_BE/src/main/java/com/example/starlet_be/domains/S3/Config/S3Config.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/S3/Config/S3Config.java
@@ -7,45 +7,45 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
 
-    @Value("${s3.bucket}")
-    private String bucket;
-
     @Value("${s3.region}")
     private String region;
 
-    @Value("${s3.access-key}")
+    @Value("${s3.access-key:}")
     private String accessKey;
 
-    @Value("${s3.secret-key}")
+    @Value("${s3.secret-key:}")
     private String secretKey;
 
 
     @Bean
     public S3Presigner s3Presigner() {
-        return S3Presigner.builder()
-                .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)
-                        )
-                )
-                .build();
+        S3Presigner.Builder builder = S3Presigner.builder()
+                .region(Region.of(region));
+
+        if (!accessKey.isBlank() && !secretKey.isBlank()) {
+            builder.credentialsProvider(
+                    StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey))
+            );
+        }
+        return builder.build();
     }
 
     @Bean
     public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)
-                        )
-                )
-                .build();
+        S3ClientBuilder builder = S3Client.builder()
+                .region(Region.of(region));
+
+        if (!accessKey.isBlank() && !secretKey.isBlank()) {
+            builder.credentialsProvider(
+                    StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey))
+            );
+        }
+        return builder.build();
     }
 }
