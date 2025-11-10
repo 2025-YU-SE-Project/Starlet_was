2534a8d2751cad8b27e20844b4ca6170b2d25307
nahyun <nahyun040411@naver.com>
2025-11-11 01:27:52 +0900

    feat : 마이페이지 프로필 사진 확정 dto

---
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/mypage/dto/ConfirmPhotoResDto.java
@@ -0,0 +1,10 @@
+package com.example.starlet_be.domains.mypage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(staticName = "of")
+public class ConfirmPhotoResDto {
+    private String url;
+}
