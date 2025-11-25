0a5eaf835978680ed3c737274fdea2e11bd53a81
nahyun <nahyun040411@naver.com>
2025-11-25 22:16:46 +0900

    docs : 친구 기능 관련 swagger 작성

--- Starlet_BE/src/main/java/com/example/starlet_be/domains/friend/dto/FriendSearchResDto.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/friend/dto/FriendSearchResDto.java
@@ -1,5 +1,6 @@
 package com.example.starlet_be.domains.friend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,9 @@ public class FriendSearchResDto {
     private String nickname;
     private String profileUrl;
 
+    @Schema(
+            description = "친구 상태 -> 가능한 값: PENDING(대기), ACCEPTED(친구됨), NONE(관계 없음)"
+    )
     private String status;
 
     private Long remainingSeconds;
