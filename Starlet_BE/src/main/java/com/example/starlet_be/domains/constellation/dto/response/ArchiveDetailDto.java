036fd95317ea2075cf5b5f34e1a43f01c09fb730
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-11-25 23:08:09 +0900

    feat : Constellation Response DTO 설명 추가

--- Starlet_BE/src/main/java/com/example/starlet_be/domains/constellation/dto/response/ArchiveDetailDto.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/constellation/dto/response/ArchiveDetailDto.java
@@ -2,6 +2,7 @@ package com.example.starlet_be.domains.constellation.dto.response;
 
 import com.example.starlet_be.domains.connection.dto.response.ConnectionDto;
 import com.example.starlet_be.domains.star.dto.response.StarArchiveDetailDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,17 +12,44 @@ import java.util.List;
 @Getter
 @Builder
 public class ArchiveDetailDto {
+
+    @Schema(example = "1")
     private Long constellationId;
+
+    @Schema(example = "bottle con")
     private String name;
+
+    @Schema(example = "this is bottle con")
     private String description;
+
+    @Schema(example = "2025-11-11")
     private LocalDate date;
+
+    @Schema(example = "true")
     private Boolean isRepresentative;
+
+    @Schema(example = "StarArchiveDetailDto")
     private List<StarArchiveDetailDto> stars;
+
+    @Schema(example = "")
     private List<ConnectionDto> connections;
+
+    @Schema(example = "1")
     private Integer happynessCount;
+
+    @Schema(example = "2")
     private Integer funnyCount;
+
+    @Schema(example = "3")
     private Integer neutralCount;
+
+    @Schema(example = "4")
     private Integer surprisingCount;
+
+    @Schema(example = "5")
     private Integer angerCount;
+
+    @Schema(example = "6")
     private Integer sadnessCount;
+
 }
--- Starlet_BE/src/main/java/com/example/starlet_be/domains/constellation/dto/response/ConstellationNameSuggestDto.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/constellation/dto/response/ConstellationNameSuggestDto.java
@@ -1,11 +1,16 @@
 package com.example.starlet_be.domains.constellation.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class ConstellationNameSuggestDto {
+
+    @Schema(example = "bottle con")
     private String name;
+
+    @Schema(example = "this is bottle con")
     private String description;
 }
