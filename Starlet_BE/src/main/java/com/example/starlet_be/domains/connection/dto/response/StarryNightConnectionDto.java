2dd0b84795aada80eea20dd71cf00dbbee102396
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-11-25 23:09:15 +0900

    feat : Constellation Response DTO 설명 추가

--- Starlet_BE/src/main/java/com/example/starlet_be/domains/connection/dto/response/StarryNightConnectionDto.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/connection/dto/response/StarryNightConnectionDto.java
@@ -1,12 +1,20 @@
 package com.example.starlet_be.domains.connection.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class StarryNightConnectionDto {
+
+
+    @Schema(example = "1")
     private Long connectionId;
+
+    @Schema(example = "2")
     private Long startStarId;
+
+    @Schema(example = "3")
     private Long endStarId;
 
     @Builder
