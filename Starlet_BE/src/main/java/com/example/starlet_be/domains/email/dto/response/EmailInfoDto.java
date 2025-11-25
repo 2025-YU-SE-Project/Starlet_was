53a39acd23e3449f0ed90871d639b4b2f7bbe30c
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-11-25 23:00:19 +0900

    feat : Email Response DTO 설명 추가

--- Starlet_BE/src/main/java/com/example/starlet_be/domains/email/dto/response/EmailInfoDto.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/email/dto/response/EmailInfoDto.java
@@ -1,13 +1,22 @@
 package com.example.starlet_be.domains.email.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class EmailInfoDto {
+
+    @Schema(example = "1")
     private Long emailId;
+
+    @Schema(example = "starlet@example.com")
     private String emailAddress;
+
+    @Schema(example = "VERIFY")
     private String verifyType;
+
+    @Schema(example = "null")
     private String verifyExpireAt;
 
     @Builder public EmailInfoDto(Long emailId, String emailAddress, String verifyType, String verifyExpireAt) {
