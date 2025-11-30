10e6e9e0c2d0f04a326ddf9cbeaa97ee477c218a
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-11-30 12:41:13 +0900

    feat : 마이페이지 정보 수정 닉네임 글자 수 변경
    요구사항 변경으로 인해 닉네임 최대 길이 수를 조절하였습니다.

--- Starlet_BE/src/main/java/com/example/starlet_be/domains/mypage/dto/request/UpdateNicknameReqDto.java
+++ Starlet_BE/src/main/java/com/example/starlet_be/domains/mypage/dto/request/UpdateNicknameReqDto.java
@@ -7,6 +7,6 @@ import lombok.Getter;
 @Getter
 public class UpdateNicknameReqDto {
     @NotBlank(message = "닉네임을 입력하세요.")
-    @Size(min = 2, max = 10, message = "닉네임은 2~10자여야 합니다.")
+    @Size(min = 2, max = 6, message = "닉네임은 2~6자여야 합니다.")
     private String nickname;
 }
